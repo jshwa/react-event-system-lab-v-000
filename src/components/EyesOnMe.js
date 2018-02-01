a9c1d59a20c267fc75b66ed596cf012e0d90c215
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-01 22:28:26 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 class EyesOnMe extends React.Component{
 
+  render(){
+    return(
+      <button onFocus={() => {console.log('Good!')} onBlur={() => {console.log('Hey! Eyes on me!')}}}/>
+    )
+  }
 }
 
 export default EyesOnMe
