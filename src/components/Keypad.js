bbec7f774e2df2aa65d163fa31074bd794a36bbd
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-01 22:29:55 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ class Keypad extends React.Component{
 
   render(){
     return (
-    <input type='password' onkeyUp={()=>{console.log('Entering password...')}}/>
+    <input type='password' onKeyUp={()=>{console.log('Entering password...')}}/>
     )
   }
 }
